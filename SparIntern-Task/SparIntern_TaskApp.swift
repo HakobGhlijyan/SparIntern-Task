b8bfef5975a6ff172d596fa6d35588422f0fb179
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-08-09 20:55:00 +0400

    Initial Commit

---
+++ SparIntern-Task/SparIntern_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparIntern_TaskApp.swift
+//  SparIntern-Task
+//
+//  Created by Hakob Ghlijyan on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparIntern_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
